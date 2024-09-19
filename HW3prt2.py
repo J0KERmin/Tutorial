5d2ae142ad0d10406a3777e778571efa6b1b6c7a
MarynaZubova <134328732+J0KERmin@users.noreply.github.com>
2024-09-19 21:54:31 +0300

    Add files via upload

---
+++ HW3prt2.py
@@ -0,0 +1,29 @@
+import time
+import multiprocessing
+
+def get_factors(n):
+    factors = [1]
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            factors.append(i)
+            if i != n // i:
+                factors.append(n // i)
+    factors.append(n)
+    return sorted(factors)
+
+def parallel_factorize(numbers):
+    with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+        return pool.map(get_factors, numbers)
+
+# Перевірка функції
+if __name__ == "__main__":
+    start_time = time.time()
+    a, b, c, d = parallel_factorize([128, 255, 99999, 10651060])
+    end_time = time.time()
+
+    print("Parallel execution time:", end_time - start_time)
+
+    assert a == [1, 2, 4, 8, 16, 32, 64, 128]
+    assert b == [1, 3, 5, 15, 17, 51, 85, 255]
+    assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+    assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
